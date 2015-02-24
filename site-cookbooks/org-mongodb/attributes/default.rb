91aed76242f91cf17381101e2326cc50c318735d
Ian D. Rossi <ian.d.rossi@aimtheory.com>
2015-02-24 12:07:40 -0500

    [ org-mongodb ] Working cookbook

---
+++ site-cookbooks/org-mongodb/attributes/default.rb
@@ -0,0 +1,6 @@
+default[:mongodb][:config] = { 
+  :dbpath => '/opt/org/data',
+  :logpath => '/opt/org/log/mongodb.log',
+  :bind_ip => '0.0.0.0',
+  :port => '29019'
+}
---
+++ site-cookbooks/org-mongodb/recipes/default.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook Name:: org-mongodb
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+include_recipe "mongodb::default"
